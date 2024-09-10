0ea5329308d008cd3cf0aa2bde9105aa3121d4c5
julia_vostrik <kosheen67@gmail.com>
2024-09-10 21:50:04 +0300

    Исправляет код для Бургер меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,17 +2,17 @@ const burgerLine = document.querySelector('.burger__line');
 const navList = document.querySelector('.main-nav__list');
 
 burgerLine.addEventListener('click', () => {
-  if (burgerLine.classList.contains('burger__line--closed-menu') & navList.classList.contains('.main-nav__list--closed-menu')) {
-    burgerLine.classList.remove('.burger__line--closed-menu');
-    burgerLine.classList.add('.burger__line--opened-menu');
+  if (burgerLine.classList.contains('burger__line--closed-menu') & navList.classList.contains('main-nav__list--closed-menu')) {
+    burgerLine.classList.remove('burger__line--closed-menu');
+    burgerLine.classList.add('burger__line--opened-menu');
 
-    navList.classList.remove('.main-nav__list--closed-menu');
-    navList.classList.add('.main-nav__list--opened-menu');
+    navList.classList.remove('main-nav__list--closed-menu');
+    navList.classList.add('main-nav__list--opened-menu');
   } else {
-    burgerLine.classList.add('.burger__line--closed-menu');
-    burgerLine.classList.remove('.burger__line--opened-menu');
+    burgerLine.classList.add('burger__line--closed-menu');
+    burgerLine.classList.remove('burger__line--opened-menu');
 
-    navList.classList.add('.main-nav__list--closed-menu');
-    navList.classList.remove('.main-nav__list--opened-menu');
+    navList.classList.add('main-nav__list--closed-menu');
+    navList.classList.remove('main-nav__list--opened-menu');
   }
 });
