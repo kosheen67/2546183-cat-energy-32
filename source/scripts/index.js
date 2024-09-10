d4923b3490e0fc32f82973a3ae8e3221b7c412fd
kosheen67 <103139602+kosheen67@users.noreply.github.com>
2024-09-10 20:02:32 +0300

    Module5-task1 (#9)
    * Добавляет первые некоторые декоративные изображения
    
    * Добавляет адаптивные картинки худого, толстого кота и пин на карте
    
    * Добавляет всю векторную графику
    
    * Вносит корректировки наставника
    
    * Исправляет ошибку по линтеру
    
    * Добавляет адаптивную сетку шапки
    
    * Добавляет адаптивную сетку шапки
    
    * Добавляет адаптивную сетку до Примера на мобильной версии
    
    * Добавляет 2ю часть адапт верстки для моб версии
    
    * Добавляет адапт верстку для мобильной версии
    
    * Вносит небольшие изменения к адапт верстке для мобилы
    
    * Вносит небольшие изменения
    
    * Добавляет  часть адапт сетки для моб для стр Каталог
    
    * Добавляет адапт сетки для моб версии стр Каталог
    
    * Добавляет адапт сетки для главной страницы планшета
    
    * Добавляет адапт сетки для планшета
    
    * Добавляет адапт сетки для планшета
    
    * Добавляет адапт сетки для десктопа
    
    * Добавляет адапт сетки Каталога для десктопа
    
    * Исправляет ошибки
    
    * Исправляет ошибки 2
    
    * Добавляет переключение меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,18 @@
-/* в этот файл добавляет скрипты*/
+const burgerLine = document.querySelector('.burger__line');
+const navList = document.querySelector('.main-nav__list');
+
+burgerLine.addEventListener('click', () => {
+  if (burgerLine.classList.contains('burger__line--closed-menu') & navList.classList.contains('.main-nav__list--closed-menu')) {
+    burgerLine.classList.remove('.burger__line--closed-menu');
+    burgerLine.classList.add('.burger__line--opened-menu');
+
+    navList.classList.remove('.main-nav__list--closed-menu');
+    navList.classList.add('.main-nav__list--opened-menu');
+  } else {
+    burgerLine.classList.add('.burger__line--closed-menu');
+    burgerLine.classList.remove('.burger__line--opened-menu');
+
+    navList.classList.add('.main-nav__list--closed-menu');
+    navList.classList.remove('.main-nav__list--opened-menu');
+  }
+});
