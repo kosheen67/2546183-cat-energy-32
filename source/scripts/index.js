95d65343240f2e5e90b3f387036ff31f2cb43704
kosheen67 <103139602+kosheen67@users.noreply.github.com>
2024-09-18 01:26:22 +0300

    Исправляет некоторые части

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,19 +1,26 @@
 const burgerLine = document.querySelector('.burger__line');
 const navList = document.querySelector('.main-nav__list');
+const catalogList = document.querySelector('.catalog-nav__list');
 
 burgerLine.addEventListener('click', () => {
-  if (burgerLine.classList.contains('burger__line--closed-menu') & navList.classList.contains('main-nav__list--closed-menu')) {
+  if (burgerLine.classList.contains('burger__line--closed-menu') & navList.classList.contains('main-nav__list--closed-menu') & catalogList.classList.contains('catalog-nav__list--closed-menu')) {
     burgerLine.classList.remove('burger__line--closed-menu');
     burgerLine.classList.add('burger__line--opened-menu');
 
     navList.classList.remove('main-nav__list--closed-menu');
     navList.classList.add('main-nav__list--opened-menu');
+
+    catalogList.classList.remove('catalog-nav__list--closed-menu');
+    catalogList.classList.add('catalog-nav__list--opened-menu');
   } else {
     burgerLine.classList.add('burger__line--closed-menu');
     burgerLine.classList.remove('burger__line--opened-menu');
 
     navList.classList.add('main-nav__list--closed-menu');
     navList.classList.remove('main-nav__list--opened-menu');
+
+    catalogList.classList.add('catalog-nav__list--closed-menu');
+    catalogList.classList.remove('catalog-nav__list--opened-menu');
   }
 });
 
